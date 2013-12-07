89c0de87e5a4a7084fe7984b3e75173abf5d2a2a
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2013-12-07 19:17:00 +0100

    Path to test file fixed

--- src/Demo.java
+++ src/Demo.java
@@ -1,12 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Demo {
 	public static void main(String[] args) {
+		File directory = new File (".");
 		Road road = new Road();
-        Graph graph = road.getWeightsFromFile("D:\\Kody\\java2\\CSO\\test");
-		CockroachNest problem = new CockroachNest(graph, 200,3,2,100);
-		CockroachNest problem2 = new CockroachNest(graph);
-		long start = System.currentTimeMillis();
-		problem.solve();
-		problem2.solve();
-		System.out.println("Time spent: " + (System.currentTimeMillis() - start) + " ms.");
+        Graph graph;
+		try {
+			graph = road.getWeightsFromFile(directory.getCanonicalPath() + "\\test");
+			CockroachNest problem = new CockroachNest(graph, 200,3,2,100);
+			CockroachNest problem2 = new CockroachNest(graph);
+			long start = System.currentTimeMillis();
+			problem.solve();
+			problem2.solve();
+			System.out.println("Time spent: " + (System.currentTimeMillis() - start) + " ms.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
 }
